c93759271cfa53d97af4e77ee76e5deb8f284972
admin <admin@admin-PC>
2015-01-24 10:27:30 +0800

    chage to ueditor, instead of umeditor

--- app/controllers/PostController.java
+++ app/controllers/PostController.java
@@ -1,6 +1,5 @@
 package controllers;
 
-
 import global.Consts;
 
 import java.util.Date;
@@ -14,81 +13,81 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import service.ArticleService;
 
-public class PostController extends Controller{
-	
+public class PostController extends Controller {
+
 	/**
 	 * render the post page
+	 * 
 	 * @return
 	 */
 	public static Result render() {
-		if(!StringUtils.equals(session("authenticated"), Consts.TRUE)) {
-			return redirect(
-		            routes.SigninController.signin()
-		        );
+		if (!StringUtils.equals(session("authenticated"), Consts.TRUE)) {
+			return redirect(routes.SigninController.signin());
 		}
-    	return ok(views.html.post.post.render(Form.form(Post.class)));
-    }
-	
+		return ok(views.html.post.post.render(Form.form(Post.class)));
+	}
+
 	/**
 	 * persist the article
+	 * 
 	 * @return
 	 */
 	public static Result post() {
 		Form<Post> article = Form.form(Post.class).bindFromRequest();
-	    if (article.hasErrors()) {
-	        return badRequest(views.html.post.post.render(article));
-	    } else {
-	    	Post post = article.get();
-	    	Article articlePo = new Article(post.title, post.content, "winry", new Date());
-	    	ArticleService.saveArticle(articlePo);
-	        return redirect(
-	            routes.Application.index(0)
-	        );
-	    }
+		if (article.hasErrors()) {
+			return badRequest(views.html.post.post.render(article));
+		} else {
+			Post post = article.get();
+			Article articlePo = new Article(post.title, post.content, "winry",
+					new Date());
+			articlePo.status = "p";
+			ArticleService.saveArticle(articlePo);
+			return redirect(routes.Application.index(0));
+		}
 	}
-	
+
 	/**
 	 * render the edit page
+	 * 
 	 * @return
 	 */
 	public static Result renderEdit(long id) {
 		Article article = Article.findById(id);
 		return ok(views.html.post.edit.render(Form.form(Post.class), article));
 	}
-	
+
 	/**
 	 * save the change to article
+	 * 
 	 * @return
 	 */
 	public static Result saveChange() {
 		Form<Post> article = Form.form(Post.class).bindFromRequest();
-	    if (article.hasErrors()) {
-	        return badRequest(views.html.post.post.render(article));
-	    } else {
-	    	Post post = article.get();
-	    	Article articlePo = Article.findById(post.id);
-	    	articlePo.title = post.title;
-	    	articlePo.content = post.content;
-	    	ArticleService.updateArticle(articlePo);
-	        return redirect(
-	            routes.Application.index(0)
-	        );
-	    }
+		if (article.hasErrors()) {
+			return badRequest(views.html.post.post.render(article));
+		} else {
+			Post post = article.get();
+			Article articlePo = Article.findById(post.id);
+			articlePo.title = post.title;
+			articlePo.content = post.content;
+			ArticleService.updateArticle(articlePo);
+			return redirect(routes.Application.index(0));
+		}
 	}
-	
+
 	public static class Post {
 
 		public long id;
-	    public String title;
-	    public String content;
-	    
-	    public String validate() {
-		    if (null == title) {
-		      return "title can not be empty";
-		    }
-		    return null;
+		public String title;
+		public String content;
+
+		public String validate() {
+			if (null == title) {
+				return "title can not be empty";
+			}
+			return null;
 		}
 
 	}
-	
+
 }
